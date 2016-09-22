974fabf05981f4a0b76efd376528a76a4eb7dfde
FluffyTheDestroyerOfWorlds <dboesen@e112-m-21.mchenry.edu>
2016-09-22 12:20:54 -0500

    CityGuide
    This uses a collection and opens 2 pages and 3 pictures based on user
    clicks

---
+++ app/src/main/java/com/bigskysolutionsinc/dboesen/cityguide/Pier.java
@@ -0,0 +1,13 @@
+package com.bigskysolutionsinc.dboesen.cityguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pier extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pier);
+    }
+}
---
+++ app/src/main/java/com/bigskysolutionsinc/dboesen/cityguide/Water.java
@@ -0,0 +1,13 @@
+package com.bigskysolutionsinc.dboesen.cityguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Water extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water);
+    }
+}
